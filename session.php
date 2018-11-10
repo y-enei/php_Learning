aef5577e193bc29553fcb0b187c28fb836842d4b
ENEI Yoshimasa <y-enei@jdserve.co.jp>
2018-11-10 09:24:07 +0900

    fixed some bugs

--- session.php
+++ session.php
@@ -4,10 +4,10 @@
   $_SESSION["pass"] = $_POST["pass"];
 
   //ユーザIDとパスワードのどちらかがカラではないことをチェック
-  if((!isset($_SESSION["loginname"])) || (!isset($_SESSION["userid"]))){
+  if((!isset($_SESSION["loginname"])) || (!isset($_SESSION["pass"]))){
     ?>
     ユーザIDまたはパスワードが入力されていません。<br />
-    <a href="login.html">セッション生成ページ</a>
+    <a href="login.html">ログインページ</a>
     <?php
     exit;
   }
@@ -17,7 +17,7 @@
   if(!(preg_match("/^[a-zA-Z0-9]+$/", $_SESSION["loginname"])))){
     ?>
       ユーザIDは半角英数字で入力してください。<br />
-      <a href="login.html">セッション生成ページ</a>
+      <a href="login.html">ログインページ</a>
       <?php
       exit;
   }
@@ -27,12 +27,11 @@
   if($_SESSION["loginname"] != "enei" || $_SESSION["pass"] != "pass"){
     ?>
       ログインに失敗しました。<br />
-      <a href="login.html">セッション生成ページ</a>
+      <a href="login.html">ログインページ</a>
       <?php
       exit;
   }
   if(isset($_POST["userid"])) setcookie("userid", $_POST["userid"], time()+120);
 ?>
 会員専用画面です。<br />
-ログイン認証に成功しました。現在ログインしている状態です。<br />
 <a href="cookie.php">マイページへ</a>
