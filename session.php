66db0f1bdfc6eca9f00f92c44ad69a251d80f224
ENEI Yoshimasa <y-enei@jdserve.co.jp>
2018-11-10 09:14:32 +0900

    add input check for user-id

--- session.php
+++ session.php
@@ -3,6 +3,27 @@
   $_SESSION["loginname"] = $_POST["userid"];
   $_SESSION["pass"] = $_POST["pass"];
 
+  //ユーザIDとパスワードのどちらかがカラではないことをチェック
+  if((!isset($_SESSION["loginname"])) || (!isset($_SESSION["userid"]))){
+    ?>
+    ユーザIDまたはパスワードが入力されていません。<br />
+    <a href="login.html">セッション生成ページ</a>
+    <?php
+    exit;
+  }
+?>
+<?php
+  //ユーザIDが半角英数字のみであることをチェック
+  if(!(preg_match("/^[a-zA-Z0-9]+$/", $_SESSION["loginname"])))){
+    ?>
+      ユーザIDは半角英数字で入力してください。<br />
+      <a href="login.html">セッション生成ページ</a>
+      <?php
+      exit;
+  }
+?>
+
+<?php
   if($_SESSION["loginname"] != "enei" || $_SESSION["pass"] != "pass"){
     ?>
       ログインに失敗しました。<br />
